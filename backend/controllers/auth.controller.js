fb4c4db7512975eab2174f060f31e9ca5c33a7b4
tobe <tobemarizu@gmail.com>
2025-05-20 14:08:43 +0100

    bcrypt issue

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
 import User from "../models/user.model.js";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
     try {
